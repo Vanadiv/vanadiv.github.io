f29d3fe17a9b350a167d8ece59dd6cabe9668491
Диана Ишкильдина <79214212+Vanadiv@users.noreply.github.com>
2024-11-19 19:56:09 +0300

    Create Info.vue

---
+++ src/pages/Info.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class = "wrap">
+        <h1> Дополнительная информация о погоде</h1>
+        <router-link to="/">
+            <button> Назад </button>
+        </router-link>
+    </div>
+</template>
+
+<style scoped>
+.wrap {
+    display: flex;
+    justify-content: center;
+}
+button {
+    border-radius: 10px;
+    border: 2px solid #0d0b87;
+    padding: 10px 15px;
+    margin-left: 20px;
+    cursor: pointer;
+    transition: transfor, 500ms ease;
+    background: #0d0b87;
+    color: #fff;
+}
+button:hover {
+    transform: scale(1.1) translateY(-5px);
+}
+</style>
